c28d56edf541fa2d25f593e31599dcf4a5b3416b
Dean Hiller <dean@alvazan.com>
2013-07-11 10:16:14 -0600

    add a port thing in

--- webapp/app/nosql/NoSqlConfigImpl.java
+++ webapp/app/nosql/NoSqlConfigImpl.java
@@ -2,6 +2,8 @@ package nosql;
 
 import java.util.Map;
 
+import play.Play;
+
 import com.alvazan.orm.api.base.Bootstrap;
 import com.alvazan.play.NoSqlConfig;
 
@@ -9,6 +11,8 @@ public class NoSqlConfigImpl implements NoSqlConfig {
 
 	@Override
 	public void configure(Map<String, Object> props) {
+		String portStr = (String) Play.configuration.get("nosql.cassandra.thriftport");
+		props.put(Bootstrap.CASSANDRA_THRIFT_PORT, portStr);
 		props.put(Bootstrap.CASSANDRA_CF_CREATE_CALLBACK, new OurCfMetaCallback());
 	}
 
