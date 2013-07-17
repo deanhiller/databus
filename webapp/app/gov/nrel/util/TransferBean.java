0fd6d7f30c8e464de61d1b7ad1df3e73fb41bae7
Dean Hiller <dean@alvazan.com>
2013-07-17 10:01:09 -0600

    transfer the raw data is working here now

--- webapp/app/gov/nrel/util/TransferBean.java
+++ webapp/app/gov/nrel/util/TransferBean.java
@@ -1,9 +1,13 @@
 package gov.nrel.util;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+
+import javax.persistence.Column;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,10 +16,19 @@ import com.alvazan.orm.api.base.Bootstrap;
 import com.alvazan.orm.api.base.DbTypeEnum;
 import com.alvazan.orm.api.base.NoSqlEntityManager;
 import com.alvazan.orm.api.base.NoSqlEntityManagerFactory;
+import com.alvazan.orm.api.base.anno.NoSqlEmbeddable;
+import com.alvazan.orm.api.base.anno.NoSqlEntity;
 import com.alvazan.orm.api.z3api.NoSqlTypedSession;
 import com.alvazan.orm.api.z3api.QueryResult;
+import com.alvazan.orm.api.z5api.NoSqlSession;
 import com.alvazan.orm.api.z8spi.KeyValue;
+import com.alvazan.orm.api.z8spi.NoSqlRawSession;
+import com.alvazan.orm.api.z8spi.Row;
+import com.alvazan.orm.api.z8spi.conv.ByteArray;
+import com.alvazan.orm.api.z8spi.iter.AbstractCursor;
 import com.alvazan.orm.api.z8spi.iter.Cursor;
+import com.alvazan.orm.api.z8spi.meta.DboColumnMeta;
+import com.alvazan.orm.api.z8spi.meta.DboTableMeta;
 import com.alvazan.orm.api.z8spi.meta.TypedColumn;
 import com.alvazan.orm.api.z8spi.meta.TypedRow;
 import com.alvazan.play.NoSql;
@@ -30,72 +43,89 @@ public class TransferBean {
 		String upgradeMode = (String) Play.configuration.get("upgrade.mode");
 		if(upgradeMode == null || !"TRANSFER".equals(upgradeMode))
 			return; //we dont' run unless we are in transfer mode
-		
+
+		String keyspace = (String) Play.configuration.get("transfer.nosql.cassandra.keyspace");
+		String cluster = (String) Play.configuration.get("transfer.nosql.clusterName");
+		String seeds = (String) Play.configuration.get("transfer.nosql.seeds");
+		String port = (String) Play.configuration.get("transfer.nosql.port");
+
+        List<Class> classes = Play.classloader.getAnnotatedClasses(NoSqlEntity.class);
+        List<Class> classEmbeddables = Play.classloader.getAnnotatedClasses(NoSqlEmbeddable.class);
+        classes.addAll(classEmbeddables);
+        
 		Map<String, Object> props = new HashMap<String, Object>();
-		NoSqlEntityManagerFactory factory = Bootstrap.create(DbTypeEnum.CASSANDRA, props );
+		props.put(Bootstrap.TYPE, "cassandra");
+		props.put(Bootstrap.CASSANDRA_KEYSPACE, keyspace);
+		props.put(Bootstrap.CASSANDRA_CLUSTERNAME, cluster);
+		props.put(Bootstrap.CASSANDRA_SEEDS, seeds);
+		props.put(Bootstrap.CASSANDRA_THRIFT_PORT, port);
+		props.put(Bootstrap.AUTO_CREATE_KEY, "create");
+		props.put(Bootstrap.LIST_OF_EXTRA_CLASSES_TO_SCAN_KEY, classes);
+
+		NoSqlEntityManagerFactory factory = Bootstrap.create(props, Play.classloader);
 		NoSqlEntityManager mgr2 = factory.createEntityManager();
 		NoSqlEntityManager mgr = NoSql.em();
-		
-		NoSqlTypedSession session = mgr.getTypedSession();
-		NoSqlTypedSession session2 = mgr2.getTypedSession();
 
-		String cf = "DboColumnMeta";
-		portTableToNewCassandra(mgr, session, session2, cf);
-		cf = "DboTableMeta";
-		portTableToNewCassandra(mgr, session, session2, cf);
-		cf = "AppProperty";
-		portTableToNewCassandra(mgr, session, session2, cf);
-		cf = "EntityGroupXref";
-		portTableToNewCassandra(mgr, session, session2, cf);
+		String cf = "User";
+		portTableToNewCassandra(mgr, mgr2, cf);
 		cf = "KeyToTableName";
-		portTableToNewCassandra(mgr, session, session2, cf);
+		portTableToNewCassandra(mgr, mgr2, cf);
+		cf = "AppProperty";
+		portTableToNewCassandra(mgr, mgr2, cf);
+		cf = "EntityGroupXref";
+		portTableToNewCassandra(mgr, mgr2, cf);
 		cf = "SdiColumn";
-		portTableToNewCassandra(mgr, session, session2, cf);
+		portTableToNewCassandra(mgr, mgr2, cf);
 		cf = "SdiTable";
-		portTableToNewCassandra(mgr, session, session2, cf);
+		portTableToNewCassandra(mgr, mgr2, cf);
 		cf = "SecureResourceGroupXref";
-		portTableToNewCassandra(mgr, session, session2, cf);
+		portTableToNewCassandra(mgr, mgr2, cf);
 		cf = "SecurityGroup";
-		portTableToNewCassandra(mgr, session, session2, cf);
-		cf = "WebNodeDbo";
-		portTableToNewCassandra(mgr, session, session2, cf);
+		portTableToNewCassandra(mgr, mgr2, cf);
 	}
 
 	private void portTableToNewCassandra(NoSqlEntityManager mgr,
-			NoSqlTypedSession session, NoSqlTypedSession session2, String cf) {
+			NoSqlEntityManager mgr2, String cf) {
 		log.info("starting port of Column Family="+cf);
-		QueryResult result = session.createQueryCursor("select u from "+cf+" as u", 500);
-		Cursor<KeyValue<TypedRow>> cursor = result.getPrimaryViewCursor();
-
+		NoSqlTypedSession session = mgr.getTypedSession();
+		NoSqlTypedSession session2 = mgr2.getTypedSession();
+		NoSqlSession raw = session.getRawSession();
+		NoSqlSession raw2 = session2.getRawSession();
+		
+		DboTableMeta meta = mgr.find(DboTableMeta.class, cf);
+		DboTableMeta meta2 = mgr2.find(DboTableMeta.class, cf);
+		AbstractCursor<Row> rows = raw.allRows(meta, 500);
+		
 		int counter = 0;
-		while(cursor.next()) {
-			TypedRow row = cursor.getCurrent().getValue();
-			TypedRow newRow = session2.createTypedRow(cf);
-			newRow.setRowKey(row.getRowKey());
-			Collection<TypedColumn> coll = row.getColumnsAsColl();
-			Iterator<TypedColumn> iterator = coll.iterator();
-			while(iterator.hasNext()) {
-				TypedColumn col = iterator.next();
-				byte[] name = col.getNameRaw();
-				byte[] value = col.getValueRaw();
-				newRow.addColumn(name, value, null);
-			}
+		while(rows.next()) {
+			Row row = rows.getCurrent();
+			List<com.alvazan.orm.api.z8spi.action.Column> columns = new ArrayList<com.alvazan.orm.api.z8spi.action.Column>();
+			columns.addAll(row.getColumns());
+			raw2.put(meta2, row.getKey(), columns);
 
-			session2.put(cf, newRow);
+//debug code to see values of bytes easier...			
+//			ByteArray b = new ByteArray(row.getKey());
+//			int d = 5+6;
+//			for(com.alvazan.orm.api.z8spi.action.Column c : columns) {
+//				ByteArray name = new ByteArray(c.getName());
+//				ByteArray col = new ByteArray(c.getValue());
+//				int a = 5+4;
+//			}
+			
 			counter++;
 			if(counter % 50 == 0) {
-				mgr.clear();
-				session2.flush();
+				raw.clear(); //clear read cache
+				raw2.flush();
 			}
-			
+
 			if(counter % 300 == 0) {
-				log.info("done porting "+counter+" records for cf="+cf);
+				log.info("ported "+counter+" records for cf="+cf+" and still porting");
 			}
 		}
-
-		//If the last batch has less than 50, we need to flush that too....
-		mgr.clear();
-		session2.flush();
+		
+		raw.clear(); //clear read cache
+		raw2.flush();
+		log.info("done porting. count="+counter+" records for cf="+cf);
 	}
 
 }
