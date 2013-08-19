cce3b88d271b16578acddb53f56813a34316d835
Dean Hiller <dean@alvazan.com>
2013-08-19 16:22:41 -0600

    this works better out of the box if we do not start with PropertyFileSnitch and instead use SimpleSnitch for now

--- webapp/app/nosql/OurCfMetaCallback.java
+++ webapp/app/nosql/OurCfMetaCallback.java
@@ -25,13 +25,19 @@ public class OurCfMetaCallback implements CreateCfCallback {
 
 	@Override
 	public Object configureKeySpace(String keyspaceName, Object def) {
-		KeyspaceDefinition keyDef = (KeyspaceDefinition) def;
-		keyDef.setStrategyClass("org.apache.cassandra.locator.NetworkTopologyStrategy");
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("DC1", "3");
-		//map.put("strategy_options", "DC1:3");
-		keyDef.setStrategyOptions(map);
-		return keyDef;
+		//With the default cassandra settings using SimpleSnitch
+		//we cannot really do the below.  Users instead can do two things
+		//1. change cassandra.yaml to PropertyFileSnitch from SimpleSnitch
+		//2. run update keyspace databus5 with placement_strategy = 'org.apache.cassandra.locator.NetworkTopologyStrategy' and strategy_options = {DC1:3};
+		//   in cassandra-cli 
+		//then they can use the datacenter awareness stuff
+//		KeyspaceDefinition keyDef = (KeyspaceDefinition) def;
+//		keyDef.setStrategyClass("org.apache.cassandra.locator.NetworkTopologyStrategy");
+//		Map<String, String> map = new HashMap<String, String>();
+//		map.put("DC1", "3");
+//		//map.put("strategy_options", "DC1:3");
+//		keyDef.setStrategyOptions(map);
+		return def;
 	}
 
 }
