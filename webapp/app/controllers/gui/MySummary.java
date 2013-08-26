df75f739af26a24a38a4fcc9745702d637edd7fe
Dean Hiller <dean@alvazan.com>
2013-08-26 09:52:02 -0600

    fix up delete groups so this works now

--- webapp/app/controllers/gui/MySummary.java
+++ webapp/app/controllers/gui/MySummary.java
@@ -29,13 +29,10 @@ public class MySummary extends Controller {
 		List<SecureSchema> databases = SecureSchema.findAll(NoSql.em());
 
 		for (SecureSchema s : databases) {
-			if("bacnet".equals(s.getName())) {
-				log.info("bacnet database");
-			}
 			List<SecureResourceGroupXref> refs = s.getEntitiesWithAccess();
 			for (SecureResourceGroupXref ref : refs) {
 				Entity entity = ref.getUserOrGroup();
-				if (log.isInfoEnabled() && "bacnet".equals(s.getName()))
+				if (log.isInfoEnabled())
 					log.info("entity=" + entity+" perm="+ref.getPermission());
 			}
 		}
