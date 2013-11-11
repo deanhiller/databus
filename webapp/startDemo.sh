c3538da092f066c7363e28da8a472181fb45c0de
Dean Hiller <dean@alvazan.com>
2013-11-11 12:58:12 -0700

    fix the startup scripts as well

--- webapp/startDemo.sh
+++ webapp/startDemo.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-play-1.2.5/play run --%demo -Xmx1024M 
+cp conf/prod.logback.xml conf/logback.xml
+cp conf/application.conf.qa conf/application.conf
+
+play-1.2.5/play run -Xmx1024M 
 #-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=1044
--- webapp/startQa.sh
+++ webapp/startQa.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+cp conf/prod.logback.xml conf/logback.xml
+cp conf/application.conf.qa conf/application.conf
+
 #NOTE: Developers, PLEASE do not start this script and start using the 
 #production database!!!!
-play-1.2.5/play run --%qa -Xmx1024M 
+play-1.2.5/play run -Xmx1024M 
 #-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=1044
