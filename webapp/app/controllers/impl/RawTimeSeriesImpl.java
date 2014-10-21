477b35dbd4c2bf3ac419c677288be996f817fd06
jcollinsnrel <justin.collins@nrel.gov>
2014-10-21 15:39:15 -0600

    fix delete table to remove it from the schemas index

--- webapp/app/controllers/impl/RawTimeSeriesImpl.java
+++ webapp/app/controllers/impl/RawTimeSeriesImpl.java
@@ -81,27 +81,30 @@ public class RawTimeSeriesImpl  extends PlayPlugin {
 		this.timeColumn = rowMeta.getTimeColumn();
 		this.valueColumn = rowMeta.getValueColumn();
 		
-		this.start = start;
-		this.end = end;
 		loadPartitions(meta);
-		partitionSize = meta.getTimeSeriesPartionSize();
-		currentIndex = partition(partitionSize);
-
-		if(start != null)
-			this.startBytes = meta.getIdColumnMeta().convertToStorage2(new BigInteger(start+""));
-		if(end != null)
-			this.endBytes = meta.getIdColumnMeta().convertToStorage2(new BigInteger(end+""));
-
-		for (DboColumnMeta thismeta:meta.getAllColumns())
-			colMeta.add(thismeta);
-		if (log.isInfoEnabled())
-			log.info("Setting up for reading partitions, partId="+currentIndex+" partitions="+existingPartitions+" start="+start);
-		
-		//jsc fire off read thread
-		String buffersizeString = Play.configuration.getProperty("databus.preread.buffer.size", "2000");
-		buffersize = Integer.parseInt(buffersizeString);
-		itemsQueue = new LinkedBlockingQueue<TSRelational>(buffersize);
-		new Thread(new AgressivePreReader()).start();
+		if (start != null && end != null) {
+			this.start = start;
+			this.end = end;
+			
+			partitionSize = meta.getTimeSeriesPartionSize();
+			currentIndex = partition(partitionSize);
+	
+			if(start != null)
+				this.startBytes = meta.getIdColumnMeta().convertToStorage2(new BigInteger(start+""));
+			if(end != null)
+				this.endBytes = meta.getIdColumnMeta().convertToStorage2(new BigInteger(end+""));
+	
+			for (DboColumnMeta thismeta:meta.getAllColumns())
+				colMeta.add(thismeta);
+			if (log.isInfoEnabled())
+				log.info("Setting up for reading partitions, partId="+currentIndex+" partitions="+existingPartitions+" start="+start);
+			
+			//jsc fire off read thread
+			String buffersizeString = Play.configuration.getProperty("databus.preread.buffer.size", "2000");
+			buffersize = Integer.parseInt(buffersizeString);
+			itemsQueue = new LinkedBlockingQueue<TSRelational>(buffersize);
+			new Thread(new AgressivePreReader()).start();
+		}
 	}
 	
 	public void deleteRange() {
